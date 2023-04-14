6cfb97905f9c3ed5407445760ee46f5ff349494a
Yan Tavares <yantdo1@gmail.com>
2023-04-14 08:37:11 -0300

    added public folder

--- projeto/src/server.ts
+++ projeto/src/server.ts
@@ -1,12 +1,21 @@
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 
 import "./database";
 import { routes } from "./routes";
 
 const app = express();
 
+const http = createServer(app);
+const io = new Server(http);
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou", socket.id);
+});
+
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => console.log("DEU BOM"));
+http.listen(3333, () => console.log("DEU BOM"));

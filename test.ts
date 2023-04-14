094bedbcfefd4da6359eb259fed3580db995fbce
Yan Tavares <yantdo1@gmail.com>
2023-04-14 07:36:24 -0300

    idk

--- test.ts
+++ test.ts
@@ -1,14 +1,4 @@
-interface Dados {
-  id: string;
-  name: string;
-}
+var x: number = 3;
+var y: number = 3;
 
-function cadastrarUser({ id, name }: Dados) {
-  console.log(id, name);
-}
-
-class Cadastro {
-  print(dados: Dados) {
-    cadastrarUser(dados);
-  }
-}
+console.log(x == y);
